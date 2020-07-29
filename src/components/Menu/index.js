35ec153320683e97b0f934840822fc7742518355
Antônio Almeida <toninetocontato@gmail.com>
2020-07-29 01:02:29 -0300

    First Commit

---
+++ src/components/Menu/index.js
@@ -0,0 +1,11 @@
+import Logo from '../../assets/img/Logo.png'
+import styled from 'styled-components';
+//import './Menu.css'
+
+const Menu = styled.header`
+    padding: 30px 40px;
+    background-color: var(--black);
+    border-bottom: 2px solid red;
+ `
+
+export default Menu
